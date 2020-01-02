1c49e83741432b396b25dc6c0d178eea5ad7d438
kateDF <kate.ff@ya.ru>
2020-01-02 13:11:11 -0500

    Add clans searching tests and data poviders

--- clashtrack-at-api/src/test/java/com/karpuk/clashtrack/api/test/ClansSearchTest.java
+++ clashtrack-at-api/src/test/java/com/karpuk/clashtrack/api/test/ClansSearchTest.java
@@ -4,6 +4,7 @@ import com.karpuk.clashtrack.api.core.model.Clan;
 import com.karpuk.clashtrack.api.core.model.ClansSearchResponse;
 import com.karpuk.clashtrack.api.test.base.SpringAwareTestBaseApi;
 import org.springframework.http.ResponseEntity;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -12,18 +13,46 @@ import java.util.Map;
 
 public class ClansSearchTest extends SpringAwareTestBaseApi {
 
-    @Test
-    public void testFilterByMinClanLevel() {
+
+    @DataProvider(name = "data-provider-min-clan-level")
+    public Object[][] dataProviderMinClassLevel() {
+        return new Object[][]{{"3"}, {"10"}, {"12"}
+        };
+    }
+
+    @DataProvider(name = "data-provider-limits")
+    public Object[][] dataProviderLimitNumberOfClans() {
+        return new Object[][]{{"100"}, {"1"}, {"0"}
+        };
+    }
+
+    @Test(dataProvider = "data-provider-min-clan-level")
+    public void testFilterByMinClanLevel(String minLevel) {
         Map<String, String> queries = new HashMap<>();
         queries.put("limit", "10");
-        queries.put("minClanLevel", "15");
+        queries.put("minClanLevel", minLevel);
 
         ResponseEntity<ClansSearchResponse> result = clanSearchService.filterClans(queries);
         softAssert.assertThat(result.getStatusCode().toString()).as("Verify status code").isEqualTo("200 OK");
 
-        int expectedLevel = Integer.parseInt(queries.get("minClanLevel"));
         List<Clan> resultClans = result.getBody().getItems();
+        int expectedLevel = Integer.parseInt(minLevel);
         softAssert.assertThat(clanSearchService.isAllClansHasNotLessLevel(resultClans, expectedLevel)).as("Verify levels").isTrue();
+        softAssert.assertAll();
+    }
+
+    @Test(dataProvider = "data-provider-limits")
+    public void testLimitOfItems(String limit) {
+        Map<String, String> queries = new HashMap<>();
+        queries.put("limit", limit);
+        queries.put("minClanLevel", "2");
+
+        ResponseEntity<ClansSearchResponse> result = clanSearchService.filterClans(queries);
+        softAssert.assertThat(result.getStatusCode().toString()).as("Verify status code").isEqualTo("200 OK");
+
+        List<Clan> resultClans = result.getBody().getItems();
+        int expectedNumOfClans = Integer.parseInt(limit);
+        softAssert.assertThat(resultClans.size()).as("Verify number of result clans").isEqualTo(expectedNumOfClans);
         softAssert.assertAll();
     }
 
